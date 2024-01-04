0c14de57b69d0490ee895508e937e83f207afa79
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2024-01-04 15:27:44 -0500

    Added picture for the hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
+import AzukiPXN from '../../public/azukipxn.png'
 
 const Hero = () => {
   return (
-    <div className='flex justify-center bg-dblue h-[50vh] place-items-center'>
+    <div className='flex justify-center bg-dblue h-[50vh] place-items-center overflow-hidden'>
         <div className='grid grid-cols-12'>
             <div className='col-span-4 col-start-3 pr-4'>
                 <div className='text-xl font-extrabold'>Welcome to SkyeGee.eth</div>
@@ -11,9 +13,11 @@ const Hero = () => {
                     This virtual space serves as a portal to the my journey through discovering Web3 and Content Creation, navigating the intersection of creativity, technology, and innovation.
                 </div>
             </div>
-            <div className='grid col-span-4 bg-gray place-items-center'>
-                this is a placeholder for an image
-            </div>
+            <Image 
+                className='col-end-11 col-span-3 overflow-hidden'
+                src={AzukiPXN}
+                alt=''
+            />
         </div>
     </div>
   )
