931e2637df0c621c60421520cd9bf5577b4eef2a
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2024-01-06 20:01:31 -0500

    Style changes to info section

--- app/components/Info.tsx
+++ app/components/Info.tsx
@@ -6,25 +6,25 @@ import PXN from '../../public/Ghost 7486.jpg'
 
 const Info = () => {
   return (
-    <div className='grid grid-cols-12 min-h-[50vh] place-items-center bg-white'>
+    <div className='grid grid-cols-12 min-h-[50vh] place-items-center bg-white py-8'>
         <div className='col-span-full flex text-center text-lg text-dblue font-extrabold'>
           Favorite Projects
         </div>
         <div className='flex col-span-8 col-start-3 justify-center text-dblue space-x-4'>
           <Image
-            className='transition duration-300 rounded-lg hover:opacity-50'
+            className='transition duration-300 rounded-lg hover:blur-sm'
             src={Avarik}
             alt=''
             width={400}
           />
           <Image
-            className='rounded-lg'
+            className='transition duration-300 rounded-lg hover:blur-sm'
             src={Azuki}
             alt=''
             width={400}
           />
           <Image
-            className='rounded-lg'
+            className='transition duration-300 rounded-lg hover:blur-sm'
             src={PXN}
             alt=''
             width={400}
