e8082a591798eff10c5841f269ca7db814e9858e
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2024-01-06 20:01:20 -0500

    Style changes to header section

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='grid grid-cols-12 justify-between bg-dblue p-4'>
         <div className='col-span-2 col-start-3'>Header Logo</div>
-        <div className='flex flex-row col-span-3 col-end-11 justify-end space-x-10'>
+        <div className='flex flex-row col-span-5 col-end-11 justify-end space-x-10'>
             <div>Header link one</div>
             <div>Header link two</div>
             <div>Header link three</div>
