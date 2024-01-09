c71926854e9579425e5c1ecb7edec9c9a539ae3a
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2024-01-08 19:54:24 -0500

    renamed quote section to projects section

---
+++ app/components/Projects.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='grid grid-cols-12 bg-swhite text-dblue text-center justify-center py-8'>
+        <div className='col-span-full text-lg font-extrabold'>
+            My Projects
+        </div>
+        <div className='flex flex-row col-start-3 col-span-8 justify-center space-x-8'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Projects
