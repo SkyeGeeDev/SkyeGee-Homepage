36559c6106a65db27127abb862398f14341503d7
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2024-01-19 13:56:18 -0500

    Fixed structure for Projects page.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,13 @@
 import Header from '../components/Header'
 import RPS from '../components/RPS'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Header />
-      <div className='h-screen grid place-content-center item-center'>
-          <RPS />
-      </div>
+      <RPS />
+      <Footer />
     </main>
   )
 }
