2183023199783986a35bdd3aebffd95880573e9e
SkyeGeeDev <skyegeedev@gmail.com>
2024-01-21 16:46:35 -0500

    Swapped header links

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
     <div className='grid grid-cols-12 justify-between bg-dblue p-4 font-permanent-marker'>
         <a href='/' className='col-span-2 col-start-3 text-lg'>SkyeGee</a>
         <div className='flex flex-row col-span-5 col-end-11 justify-end place-items-center space-x-10'>
-            <a href='/projects' className=''>Projects</a>
             <div className=''>Web3</div>
+            <a href='/projects' className=''>Projects</a>
             <div className=''>Contact Me</div>
         </div>
     </div>
