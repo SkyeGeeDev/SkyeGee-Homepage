6a94ed90e6a220c195bb2f048532d06f42d88a0f
SkyeGeeDev <skyegeedev@gmail.com>
2024-01-22 15:43:53 -0500

    Added a button to bring up the RPS game. Need to implement useState to bring up game when play game is pressed.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,22 @@
+'use client'
 import Header from '../components/Header'
 import RPS from '../components/RPS'
 import Footer from '../components/Footer'
+import { useState } from 'react'
 
 export default function Home() {
+  const [activeProject, setActiveProject] = useState('');
+
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="">
       <Header />
-      <RPS />
+      <div className='min-h-screen grid grid-cols-12 mx-[70px] pt-8'>
+        <div className='col-span-4 col-start-5 h-[500px] p-8 border-2 rounded-lg text-center bg-gray'>
+          <div className='text-lg text-white'>Rock, Paper, Scissors!</div>
+          <div className='text-md'>Play rock, paper, scissors against a computer! A Javascript game I created using React's useState hooks and onClick event handling in order to send the choice to the script as well as update the score throughout.  </div>
+          <div className='bg-gray border-2 p-4 mt-28 rounded-md cursor-pointer transition duration-300 hover:bg-blue'>Play Game!</div>
+        </div>
+      </div>
       <Footer />
     </main>
   )
