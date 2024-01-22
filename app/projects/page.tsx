f4379afc206df12511ecbb1f69cf88c0b9de6d25
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2024-01-22 16:18:23 -0500

    fixed the apos

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <div className='min-h-screen grid grid-cols-12 mx-[70px] pt-8'>
         <div className='col-span-4 col-start-5 h-[500px] p-8 border-2 rounded-lg text-center bg-gray'>
           <div className='text-lg text-white'>Rock, Paper, Scissors!</div>
-          <div className='text-md'>Play rock, paper, scissors against a computer! A Javascript game I created using React's useState hooks and onClick event handling in order to send the choice to the script as well as update the score throughout.  </div>
-          <div className='bg-gray border-2 p-4 mt-28 rounded-md cursor-pointer transition duration-300 hover:bg-blue'>Play Game!</div>
+          <div className='text-md'>Play rock, paper, scissors against a computer! A Javascript game I created using React&apos;s useState hooks and onClick event handling in order to send the choice to the script as well as update the score throughout.  </div>
+          <div  className='bg-gray border-2 p-4 mt-28 rounded-md cursor-pointer transition duration-300 hover:bg-blue'>Play Game!</div>
         </div>
       </div>
       <Footer />
