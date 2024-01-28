fb2c2b14633d58fa1fda48a67d97f7de52353947
SkyeGeeDev <140851369+SkyeGeeDev@users.noreply.github.com>
2024-01-28 02:31:02 -0500

    Changing ID to type numbers

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 import emailjs from '@emailjs/browser'
 import toast from 'react-hot-toast';
 
-const MyTextInput = ({label, ...props } : {label : string; name: string; type: string; placeholder: string; id: string}) => {
+const MyTextInput = ({label, ...props } : {label : string; name: string; type: string; placeholder: string; id: number}) => {
   const [field, meta] = useField(props);
   return (
     <div className='py-4'>
@@ -17,7 +17,7 @@ const MyTextInput = ({label, ...props } : {label : string; name: string; type: s
   );
 };
 
-const MySelect = ({label, ...props } : {label: string; name: string; id: string}) => {
+const MySelect = ({label, ...props } : {label: string; name: string; id: number}) => {
   const [field, meta] = useField(props);
   return (
     <div className='py-4'>
@@ -30,7 +30,7 @@ const MySelect = ({label, ...props } : {label: string; name: string; id: string}
   );
 };
 
-const MyTextArea = ({label, ...props} : {label: string; name: string; rows: string; placeholder: string; id: string;}) => {
+const MyTextArea = ({label, ...props} : {label: string; name: string; rows: string; placeholder: string; id: number;}) => {
   const [field, meta] = useField(props);
   return (
       <div className='py-4'>
